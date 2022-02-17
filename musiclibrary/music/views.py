722f9201abbd63485650c80b7c72b4323df975c7
Bryce Payne <bryce.payne.97@gmail.com>
2022-02-17 13:46:36 -0600

    added SongForm to views.py

--- musiclibrary/music/views.py
+++ musiclibrary/music/views.py
@@ -29,7 +29,7 @@ class SongDetail(APIView):
         except Song.DoesNotExist:
             raise Http404
 
-    def get(self,request,pk):
+    def get(self,pk):
         song = self.get_object(pk)
         serializer = SongSerializer(song)
         return Response(serializer.data)
@@ -42,21 +42,31 @@ class SongDetail(APIView):
             return Response(serializer.data)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
-    def delete(self,request, pk):
+    def delete(self, pk):
         song = self.get_object(pk)
         song.delete()
         return Response(status=status.HTTP_204_NO_CONTENT)
 
-class SongLike(APIView):
+class SongForm(APIView):
 
-    def get_object(self, pk):
-        try:
-            return Song.objects.get(pk=pk)
-        except Song.DoesNotExist:
-            raise Http404
+    def post(self, request):
+        serializer = SongSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
-    def put(self, pk):
-        song = self.get_object(pk)
-        song.likes + 1
-        song.save()
+
+# class SongLike(APIView):
+
+#     def get_object(self, pk):
+#         try:
+#             return Song.objects.get(pk=pk)
+#         except Song.DoesNotExist:
+#             raise Http404
+
+#     def put(self, pk):
+#         song = self.get_object(pk)
+#         song.likes + 1
+#         song.save()
         
